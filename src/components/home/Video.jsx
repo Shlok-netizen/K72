f95fbdfbd2ac1a45504e566fc00672ed42742c24
Shlok Singh <shloksinghshloksingh@gmail.com>
2025-08-30 23:08:48 +0530

    fixed code

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 const Video = () => {
   return (
     <div className='h-full w-full'>
-        <video className='h-full w-full object-cover' autoPlay loop muted src="../../../public/video.mp4"></video>
+        <video className='h-full w-full object-cover' autoPlay loop muted   src="/video.mp4" ></video>
     </div>
   )
 }
 
-export default Video
+export default Video;
+// src="../../../public/video.mp4"
